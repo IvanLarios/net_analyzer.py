b0f6df9c9acca33782c7687b518b414ad98a997d
Ivan Larios <ivan.larios.lopez@gmail.com>
2020-08-02 20:17:28 +0200

    Added DNS protocol detection and fixed issue where only TCP protocol was put into the traces.

--- module/net_analyzer.py
+++ module/net_analyzer.py
@@ -2,7 +2,9 @@
 
 import socket
 from scapy.all import *
+from scapy.layers import dns
 from scapy.layers.inet import IP, TCP, UDP
+
 
 
 def traffic_parser(sample_name, hostIP, sshPort):
@@ -13,6 +15,8 @@ def traffic_parser(sample_name, hostIP, sshPort):
         if packet.haslayer(TCP):
             if not(packet[TCP].sport is sshPort and packet[IP].src is hostIP) or (packet[TCP].dport is sshPort and packet[IP].dst is hostIP):
                 clean_trace.append(packet)
+        else:
+            clean_trace.append(packet)
 
     return clean_trace
 
@@ -21,24 +25,35 @@ def trace_analyzer(trace):
     protocol_list = []
     IPtemp_list = []
     prototemp_list =  []
+    DNS_list = []
     for packet in trace:
         if packet.haslayer(IP):
             IPtemp_list.append(packet[IP].src)
             IPtemp_list.append(packet[IP].dst)
             prototemp_list.append(packet.proto)
+        if packet.haslayer(DNS):
+            protocol_list.append("DNS")
+            for i in range(packet[DNS].ancount):                
+                DNS_list.append(packet[DNSRR][i].rrname)
+
+        # if packet.haslayer(IRC):
+        #    prototemp_list.append(packet.proto)
 
   
 
-
+    #Socket class has stored every protocol prefixed with "IPPROTO" with it's number given by the IANA
     table = {num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")}
     for proto in prototemp_list:
         protocol_list.append(table[proto])
     IP_list = set(IPtemp_list)
     protocol_list = set(protocol_list)
+    DNS_list = set(DNS_list)
     
     fIP = open ("C:\\PRUEBASDESO\\IP.txt", "w")
+    fDNS = open("C:\\PRUEBASDESO\\DNS.txt", "w")
     fprt = open("C:\\PRUEBASDESO\\Proto.txt", "w")
     fIP.write(str(IP_list))
+    fDNS.write(str(DNS_list))
     fprt.write(str(protocol_list))
 
 def main(file):
