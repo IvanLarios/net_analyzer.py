7562d5afc942499e5894361fdd49a6620e7ad58f
Ivan Larios <ivan.larios.lopez@gmail.com>
2020-08-04 19:20:02 +0200

    Improved IP protocol analysis function and adding TTL analysis.

--- module/net_analyzer.py
+++ module/net_analyzer.py
@@ -15,6 +15,11 @@ IRC_commands = ['ADMIN', 'AWAY', 'CNOTICE', 'CPRIVMSG', 'CONNECT', 'DIE', 'ENCAP
 
 def analyze_DNS(packet):
     auxdict = {}
+
+    #################################
+    ### Domain Queries extraction ###
+    #################################
+    
     # No comprobamos la capa DNSQR ya que el protocolo MDNS puede dar problemas ya que algunos paquetes no
     # contienen la query a la que responden.
     if packet.haslayer(DNSRR):
@@ -24,6 +29,28 @@ def analyze_DNS(packet):
             auxdict[key].append(packet[DNSRR][i].rdata)
     return auxdict
 
+def analyze_IP(packet, abnormal_list):
+    
+    ####################################
+    ### TTL Analysis ###################
+    ####################################
+
+    abFlag = 0
+    ttl = packet.ttl
+    # TTL analyzer -> Basado en "Using abnormal TTL values to detect malicious IP packets" de Yamada, R. y Goto, S.
+    if (1 < ttl <= 30) or (64 < ttl <= 98) or (128< ttl <= 225):
+        abnormal_list.append(packet)
+        abFlag = 1
+
+    ####################################
+    ### IP extraction ##################
+    ####################################
+
+    ip_dict = {}
+    ip_dict[packet[IP].src] = abFlag
+    ip_dict[packet[IP].dst] = abFlag
+
+    return ip_dict
 
 def traffic_parser(sample_name, hostIP, sshPort):
     clean_trace = []
@@ -39,16 +66,25 @@ def traffic_parser(sample_name, hostIP, sshPort):
     return clean_trace
 
 def trace_analyzer(trace):
-    IP_list = []
+    IP_list = {}
     protocol_list = []
-    IPtemp_list = []
+    TTL_suspicious_packets = []
     prototemp_list =  []
     DNS_dict = {}
     for packet in trace:
         if packet.haslayer(IP):
-            IPtemp_list.append(packet[IP].src)
-            IPtemp_list.append(packet[IP].dst)
             prototemp_list.append(packet.proto)
+            result = analyze_IP(packet, TTL_suspicious_packets)
+            # If the IPs obtained are not in the dictionary we add them
+            if result.keys() not in IP_list.keys():
+                IP_list.update(result)
+            # If the IPs are in the dictionary already we check if they were marked as suspicious
+            else:
+                for key in result.keys():
+                    if IP_list[key] != result[key]:
+                        # If they were once marked as suspicious IPs, they remain suspicious
+                        IP_list[key] = 1
+
         if packet.haslayer(DNS):
             protocol_list.append("DNS")
             DNS_dict.update(analyze_DNS(packet))
@@ -59,15 +95,13 @@ def trace_analyzer(trace):
 
     #Socket class has stored every protocol prefixed with "IPPROTO" with it's number given by the IANA
     table = {num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")}
-    for proto in prototemp_list:
+    for proto in set(prototemp_list):
         protocol_list.append(table[proto])
-    IP_list = set(IPtemp_list)
-    protocol_list = set(protocol_list)
-    
     
     fIP = open ("C:\\PRUEBASDESO\\IP.txt", "w")
     fDNS = open("C:\\PRUEBASDESO\\DNS.txt", "w")
     fprt = open("C:\\PRUEBASDESO\\Proto.txt", "w")
+
     fIP.write(str(IP_list))
     fDNS.write(str(DNS_dict))
     fprt.write(str(protocol_list))
